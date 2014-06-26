fe190dd76626a7fba2cd4655151b8b6035cb7570
duduluk101 <87215279@qq.com>
2014-06-26 10:51:37 +0800

    add gif

--- CustomTabBarDemo/CustomTabBarDemo/ForthViewController.m
+++ CustomTabBarDemo/CustomTabBarDemo/ForthViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ForthViewController.h"
+#import "CustomTabBarController.h"
+
 
 @interface ForthViewController ()
 
@@ -29,14 +31,33 @@
     // Do any additional setup after loading the view.
     self.title = @"更多";
     
-    UILabel *lbl = [[UILabel alloc] initWithFrame:CGRectMake(320/2-200/2, 150, 200, 100)];
-    lbl.text = self.title;
-    lbl.font = [UIFont boldSystemFontOfSize:100];
-    lbl.textAlignment = NSTextAlignmentCenter;
-    lbl.textColor = [UIColor grayColor];
-    [self.view addSubview:lbl];
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.frame = CGRectMake(320/2-150/2, 200, 150, 30);
+    [btn setTitle:@"隐藏tabbar" forState:UIControlStateNormal];
+    [btn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [btn setBackgroundColor:[UIColor lightGrayColor]];
+    [btn addTarget:self action:@selector(hideTab) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
+    
+    UIButton *btn2 = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn2.frame = CGRectMake(320/2-150/2, 250, 150, 30);
+    [btn2 setTitle:@"显示tabbar" forState:UIControlStateNormal];
+    [btn2 setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [btn2 setBackgroundColor:[UIColor lightGrayColor]];
+    [btn2 addTarget:self action:@selector(showTab) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn2];
+    
 
 }
+
+- (void)hideTab{
+    [self.customTabBarController hidesTabBar:YES animated:YES];
+}
+
+- (void)showTab{
+    [self.customTabBarController hidesTabBar:NO animated:YES];
+}
+
 
 - (void)didReceiveMemoryWarning
 {
